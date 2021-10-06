081065971fe14b1177bdad542375d267db52f7b1
Piotr Niełacny <piotr.nielacny@gmail.com>
2021-10-06 09:51:55 +0100

    State 0

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,3 @@
-import type { NextPage } from 'next';
-import { AppContext } from 'next/app';
-import {getRange} from "@utils/index";
-import {Dates} from '@components/Dates'
+const HelloWorld = () => <div>Hello World!</div>;
 
-const SelectDate: NextPage<{}> = (props) => {
-  const range = getRange(5)
-
-  return (
-    <div>
-      <Dates range={range}/>
-    </div>
-  );
-};
-
-export async function getServerSideProps(context: AppContext) {
-  return {
-    props: {},
-  };
-}
-
-export default SelectDate;
+export default HelloWorld;
