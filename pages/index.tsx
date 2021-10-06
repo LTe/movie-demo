fab7242b9e169b4a8a820f5fad2803834ce59e08
Piotr Niełacny <piotr.nielacny@gmail.com>
2021-10-06 11:04:33 +0100

    Show dates

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,21 @@
-const HelloWorld = () => <div>Hello World!</div>;
+import { AppContext } from 'next/app';
+import { getRange } from '@utils/index';
+import { Dates } from '@components/Dates';
 
-export default HelloWorld;
+const SelectDate = () => {
+  const range = getRange(5);
+
+  return (
+    <div>
+      <Dates range={range} />
+    </div>
+  );
+};
+
+export async function getServerSideProps(context: AppContext) {
+  return {
+    props: {},
+  };
+}
+
+export default SelectDate;
